3b3a8b7f36c1086633070e9a512309eb789ba8f4
Appyx <robert.gstoettner@icloud.com>
2017-09-13 23:33:13 +0200

    added websocket

--- executor/src/main/kotlin/at/rgstoettner/alexahome/executor/ExecutorApplication.kt
+++ executor/src/main/kotlin/at/rgstoettner/alexahome/executor/ExecutorApplication.kt
@@ -5,7 +5,16 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.client.WebSocketConnectionManager
+import org.springframework.web.socket.client.standard.StandardWebSocketClient
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.security.KeyStore
+import java.security.SecureRandom
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 
 @SpringBootApplication
@@ -14,11 +23,7 @@ class ExecutorApplication {
 }
 
 fun main(args: Array<String>) {
-    val pass = ClassPathResource("tls/pass.txt").inputStream.bufferedReader(Charsets.UTF_8).readText()
-    System.setProperty("javax.net.ssl.trustStore", ExecutorApplication::class.java.classLoader.getResource("tls/client-truststore.jks").file)
-    System.setProperty("javax.net.ssl.trustStorePassword", pass);
-    System.setProperty("javax.net.ssl.keyStore", ExecutorApplication::class.java.classLoader.getResource("tls/client-keystore.jks").file)
-    System.setProperty("javax.net.ssl.keyStorePassword", pass);
+
 
     SpringApplication.run(ExecutorApplication::class.java, *args)
 }
@@ -26,8 +31,40 @@ fun main(args: Array<String>) {
 @Component
 class Runner : CommandLineRunner {
     override fun run(vararg args: String?) {
-        val template = RestTemplate()
-        println(template.postForObject("https://localhost:8443", "hello world", String::class.java))
+        val password = ClassPathResource("tls/pass.txt").inputStream.bufferedReader(Charsets.UTF_8).readText()
+        val keyStore = KeyStore.getInstance("jks")
+        keyStore.load(ClassPathResource("tls/client-keystore.jks").inputStream, password.toCharArray())
+        val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+        keyManagerFactory.init(keyStore, password.toCharArray())
+
+        val trustStore = KeyStore.getInstance("jks")
+        trustStore.load(ClassPathResource("tls/client-truststore.jks").inputStream, password.toCharArray())
+        val trustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+        trustManagerFactory.init(trustStore)
+
+        val context = SSLContext.getInstance("SSL")
+        context.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), SecureRandom())
+
+        val client = StandardWebSocketClient()
+        client.userProperties = mapOf(Pair(org.apache.tomcat.websocket.Constants.SSL_CONTEXT_PROPERTY, context))
+
+        val handler = object : TextWebSocketHandler() {
+            override fun afterConnectionEstablished(session: WebSocketSession) {
+                session.sendMessage(TextMessage("hello server"))
+            }
+
+            override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+                println(message.payload)
+            }
+        }
+
+        val manager = WebSocketConnectionManager(client, handler, "wss://localhost:8443/wss")
+        manager.start()
+
+
+        while (true) {
+        }
+
     }
 }
 
--- skill/src/main/kotlin/at/rgstoettner/alexahome/skill/SkillApplication.kt
+++ skill/src/main/kotlin/at/rgstoettner/alexahome/skill/SkillApplication.kt
@@ -3,12 +3,21 @@ package at.rgstoettner.alexahome.skill
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketHandler
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.handler.TextWebSocketHandler
 
 
 @SpringBootApplication
@@ -26,10 +35,44 @@ fun main(args: Array<String>) {
 
 
 @RestController
-class UserController {
+class HttpsController {
     @RequestMapping(value = "/", method = arrayOf(RequestMethod.POST))
     fun user(@RequestBody test: String): String {
         println(test)
         return "hello"
+    }
+}
+
+
+@Configuration
+@EnableWebSocket
+class WebsocketConfig : WebSocketConfigurer {
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry.addHandler(myMessageHandler(), "/wss");
+    }
+
+    @Bean
+    fun myMessageHandler(): WebSocketHandler {
+        return MyMessageHandler()
+    }
+
+
+}
+
+
+@Configuration
+class MyMessageHandler : TextWebSocketHandler() {
+
+    override fun afterConnectionClosed(session: WebSocketSession?, status: CloseStatus?) {
+        // The WebSocket has been closed
+    }
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        session.sendMessage(TextMessage("hello client"))
+    }
+
+    override fun handleTextMessage(session: WebSocketSession, textMessage: TextMessage) {
+        // A message has been received
+        println("Message received: " + textMessage.payload)
     }
 }
